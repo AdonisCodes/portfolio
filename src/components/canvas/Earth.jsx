b47c047dbe337a675e2b64c39fd3d38435240b6c
AdonisCodes <s.f.businessacc@gmail.com>
2023-09-21 10:57:39 +0200

    first commit

---
+++ src/components/canvas/Earth.jsx
@@ -0,0 +1,50 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Earth = () => {
+  const earth = useGLTF("./planet/scene.gltf");
+
+  // Set default values for scale, position, and rotation
+  const defaultScale = [10, 10, 10];
+  const defaultPosition = [0, 0, 0];
+  const defaultRotation = [0, 0, 0];
+
+  return (
+    <group scale={defaultScale} position={defaultPosition} rotation={defaultRotation}>
+      <primitive object={earth.scene} />
+    </group>
+  );
+};
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
---
+++ src/constants/index.js
@@ -0,0 +1,284 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  replit,
+  fiverr,
+  shopify,
+  cookiejar,
+  superHero,
+  tripguide,
+  threejs,
+} from "../assets";
+
+import salman from "../assets/testimonials/salman.jpg";
+import guillaum from "../assets/testimonials/guillaum.png";
+import warren from "../assets/testimonials/warren.png";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+  {
+    title: "Automations Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "Freelancer",
+    icon: replit,
+    iconBg: "#383E56",
+    date: "June 2023 - Present day",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Webscraping & Automations Developer",
+    company_name: "Freelancer",
+    icon: fiverr,
+    iconBg: "#E6DEDD",
+    date: "June 2023 - Present day",
+    points: [
+      "Developing and maintaining Automation scripts using Puppeteer, Edge Functions, Selenium & requests libraries and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive edge-case handeling and ensuring undetectable compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      '1. Got the job done quickly. 2. Responsive, and thoughtful 3. A pleasure to interact with.',
+    name: "Salman Farmanfarmaian",
+    designation: "125$",
+    company: "private client",
+    image: salman,
+    stars: 5,
+    proof: 'dummy_url',
+    caseStudy: 'dummy_url',
+  },
+  {
+    testimonial:
+      "Adonis is very receptive to feedback. Just need to work on giving guarantees and having difficulties afterwards. ",
+    name: "Guillaum Racine",
+    designation: "450$",
+    company: "Ecom Store Scraper",
+    image: guillaum,
+    stars: 5,
+    proof: 'dummy_url',
+    caseStudy: 'dummy_url',
+  },
+  {
+    testimonial:
+      "This is going to by my go -to bounty hunter, incredible work. Did exactly as delivered and went above and beyond...",
+    name: "Warren Mokthrak",
+    designation: "270$",
+    company: "Simple Email Uploader",
+    image: warren,
+  },
+];
+
+const projects = [
+  {
+    name: "CookieJar",
+    description:
+      "Web-based platform that allows users to like, dislike & comment on websites. This project is for a client ( I am a fulltime contractor & client has requested I take payed partnership of the app :)",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "chakra-ui",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "zustand",
+        color: "blue-text-gradient",
+      }
+    ],
+    image: cookiejar,
+    source_code_link: "https://github.com/",
+    live_demo: "https://cookiejar.co/"
+  },
+  {
+    name: "SuperHero.",
+    description:
+      "A web-application that enables users to lookup their favourite heros. The app features stunning animations. Quick load times thanks to the SPA architecture & Svelte framework. The app will show stats on all super heros, with a simple search. This was a little dummy project for me.",
+    tags: [
+      {
+        name: "Svelte",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: superHero,
+    source_code_link: "https://github.com/",
+    live_demo: "https://super-hero-api-app-lol.onrender.com/"
+  },
+  {
+    name: "And Many more...",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Moviepy",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Selenium",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "requests",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Puppeteer",
+        color: "green-text-gradient",
+      },
+      {
+        name: "APIS",
+        color: "blue-text-gradient",
+      }
+    ],
+    image: "https://placehold.co/600x400?text=Check My Github&font=roboto",
+    source_code_link: "https://github.com/AdonisCodes",
+    live_demo: "https://github.com/AdonisCodes"
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
