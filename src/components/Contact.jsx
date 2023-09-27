659e77e85210d5a929e836133b8a8090a33f05a5
AdonisCodes <s.f.businessacc@gmail.com>
2023-09-27 10:42:43 +0200

    Added Content section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,160 +1,160 @@
-import React, { useRef, useState } from "react";
-import { motion } from "framer-motion";
-import emailjs from "@emailjs/browser";
+  import React, { useRef, useState } from "react";
+  import { motion } from "framer-motion";
+  import emailjs from "@emailjs/browser";
 
-import { styles } from "../styles";
-import { EarthCanvas } from "./canvas";
-import { SectionWrapper } from "../hoc";
-import { slideIn } from "../utils/motion";
-import GitHubCalendar from "react-github-calendar";
+  import { styles } from "../styles";
+  import { EarthCanvas } from "./canvas";
+  import { SectionWrapper } from "../hoc";
+  import { slideIn } from "../utils/motion";
+  import GitHubCalendar from "react-github-calendar";
 
-const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
-
-    setForm({
-      ...form,
-      [name]: value,
+  const Contact = () => {
+    const formRef = useRef();
+    const [form, setForm] = useState({
+      name: "",
+      email: "",
+      message: "",
     });
-  };
 
-  const selectLastHalfYear = contributions => {
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
-    const shownMonths = 13
-  
-    return contributions.filter(activity => {
-      const date = new Date(activity.date);
-      const monthOfDay = date.getMonth();
-  
-      return (
-        date.getFullYear() === currentYear &&
-        monthOfDay > currentMonth - shownMonths &&
-        monthOfDay <= currentMonth
-      );
-    });
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
+    const [loading, setLoading] = useState(false);
 
-    emailjs
-      .send(
-        'service_63r85f9',
-        "template_8x0cciz",
-        {
-          name: form.name,
-          to_name: "Simon Ferns",
-          email: form.email,
-          to_email: "s.f.businessacc@gmail.com",
-          message: form.message,
-        },
-        "CaJOJpsMV7iuCquNE"
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+    const handleChange = (e) => {
+      const { target } = e;
+      const { name, value } = target;
 
-          setForm({
-            name: "",
+      setForm({
+        ...form,
+        [name]: value,
+      });
+    };
 
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
+    const selectLastHalfYear = contributions => {
+      const currentYear = new Date().getFullYear();
+      const currentMonth = new Date().getMonth();
+      const shownMonths = 13
+    
+      return contributions.filter(activity => {
+        const date = new Date(activity.date);
+        const monthOfDay = date.getMonth();
+    
+        return (
+          date.getFullYear() === currentYear &&
+          monthOfDay > currentMonth - shownMonths &&
+          monthOfDay <= currentMonth
+        );
+      });
+    };
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setLoading(true);
 
-          alert("Ahh, something went wrong. Please try again.");
-        }
-      );
-  };
+      emailjs
+        .send(
+          'service_63r85f9',
+          "template_8x0cciz",
+          {
+            name: form.name,
+            to_name: "Simon Ferns",
+            email: form.email,
+            to_email: "s.f.businessacc@gmail.com",
+            message: form.message,
+          },
+          "CaJOJpsMV7iuCquNE"
+        )
+        .then(
+          () => {
+            setLoading(false);
+            alert("Thank you. I will get back to you as soon as possible.");
 
-  return (
-    <div
-      className={`xl:mt-12 grid xl:flex-row flex-col-reverse gap-10 overflow-hidden justify-center `}
-    >
+            setForm({
+              name: "",
 
-<motion.div variants={slideIn("right", "tween", 0.2, 1)} className='flex-[0.25]'>
-    <GitHubCalendar username="AdonisCodes"
-    labels={{
-      totalCount: `{{count}} contributions in the last year`,
-    }}
-    theme={{
-      dark: ['#1f1f1f', 'red']
-    }}
-    />
-    </motion.div>
-      <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black p-8 rounded-2xl'
-        style={{border: '1px solid white'}}
+              email: "",
+              message: "",
+            });
+          },
+          (error) => {
+            setLoading(false);
+            console.error(error);
+
+            alert("Ahh, something went wrong. Please try again.");
+          }
+        );
+    };
+
+    return (
+      <div
+        className={`xl:mt-12 grid xl:flex-row flex-col-reverse gap-10 overflow-hidden justify-center `}
       >
-        <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
-        >
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
-            <input
-              type='text'
-              name='name'
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className='bg-gray-900 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
-            <input
-              type='email'
-              name='email'
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className='bg-gray-900 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
-            <textarea
-              rows={7}
-              name='message'
-              value={form.message}
-              onChange={handleChange}
-              placeholder='What you want to say?'
-              className='bg-gray-900 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-
-          <button
-            type='submit'
-            className='bg-gray-900 py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
+  <motion.div variants={slideIn("right", "tween", 0.2, 1)} className='flex-[0.25]'>
+      <GitHubCalendar username="AdonisCodes"
+      labels={{
+        totalCount: `{{count}} contributions in the last year`,
+      }}
+      theme={{
+        dark: ['#1f1f1f', 'red']
+      }}
+      />
       </motion.div>
+        <motion.div
+          variants={slideIn("left", "tween", 0.2, 1)}
+          className='flex-[0.75] bg-black p-8 rounded-2xl'
+          style={{border: '1px solid white'}}
+        >
+          <p className={styles.sectionSubText}>Get in touch</p>
+          <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-    </div>
-  );
-};
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className='mt-12 flex flex-col gap-8'
+          >
+            <label className='flex flex-col'>
+              <span className='text-white font-medium mb-4'>Your Name</span>
+              <input
+                type='text'
+                name='name'
+                value={form.name}
+                onChange={handleChange}
+                placeholder="What's your good name?"
+                className='bg-gray-900 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              />
+            </label>
+            <label className='flex flex-col'>
+              <span className='text-white font-medium mb-4'>Your email</span>
+              <input
+                type='email'
+                name='email'
+                value={form.email}
+                onChange={handleChange}
+                placeholder="What's your web address?"
+                className='bg-gray-900 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              />
+            </label>
+            <label className='flex flex-col'>
+              <span className='text-white font-medium mb-4'>Your Message</span>
+              <textarea
+                rows={7}
+                name='message'
+                value={form.message}
+                onChange={handleChange}
+                placeholder='What you want to say?'
+                className='bg-gray-900 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              />
+            </label>
 
-export default SectionWrapper(Contact, "contact");
+            <button
+              type='submit'
+              className='bg-gray-900 py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            >
+              {loading ? "Sending..." : "Send"}
+            </button>
+          </form>
+        </motion.div>
+
+      </div>
+    );
+  };
+
+  export default SectionWrapper(Contact, "contact");
--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,37 +1,165 @@
-import { useState, useEffect } from "react";
-import YouTubePlaylist from "@codesweetly/react-youtube-playlist";
-import { motion, useAnimation } from "framer-motion";
-import { slideIn } from "../utils/motion";
+import React, { useState, useEffect } from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import axios from "axios";
+import Modal from "react-modal";
 
-function Contents() {
-    const [isLoading, setIsLoading] = useState(true);
-    const loaderControls = useAnimation();
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
 
-    useEffect(() => {
-        setIsLoading(true);
-    }, []);
+const API_KEY = "AIzaSyBbChToYFyckDdNxxawgP3zSbFYuDtpmsM";
+const CHANNEL_ID = "UC8ZHsW4tHGVavsIRSPThKsw";
+const MAX_RESULTS = 6;
 
-    const handlePlaylistLoad = () => {
-        // Trigger the animation to move the loader element out of the view
-        loaderControls.start({ x: "100%" });
-        setIsLoading(false);
+const fetchVideos = async () => {
+  try {
+    const response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=${MAX_RESULTS}`
+    );
+
+    const videos = response.data.items.map((item) => {
+      const { id, snippet } = item;
+      const { videoId } = id;
+      const { title, description, thumbnails } = snippet;
+      const thumbnail = thumbnails.medium.url;
+
+      return {
+        videoId,
+        title,
+        description,
+        thumbnail,
+      };
+    });
+
+    return videos;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+const VideoCard = ({ index, title, description, thumbnail, videoId }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalIsOpen(true);
+    console.log("open");
+  };
+
+  const handleCloseModal = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <>
+      <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+        <Tilt
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-black p-5 rounded-2xl sm:w-[360px] w-full"
+          style={{ border: "1px solid white" }}
+        >
+          <div
+            className="relative w-full h-[230px]"
+            onClick={handleOpenModal}
+            style={{ cursor: "pointer" }}
+          >
+            <img
+              src={thumbnail}
+              alt="video_thumbnail"
+              className="w-full h-full object-cover rounded-2xl"
+            />
+
+            <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+              <div className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+                <a href={`https://www.youtube.com/watch?v=${videoId}`} target="_blank">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 text-white"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      // eslint-disable-next-line max-len
+                      d="M10 0C4.477 0 0 4.477 0 10s4.477 10 10 10 10-4.477 10-10S15.523 0 10 0zm4.5 10.5l-7 5V5l7 5z"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-5">
+            <h3 className="text-white font-bold text-[24px]">{title}</h3>
+            <p className="mt-2 text-secondary text-[14px]">{description}</p>
+          </div>
+        </Tilt>
+      </motion.div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={handleCloseModal}
+        contentLabel="Video Player Modal"
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <div className="video-container">
+          <iframe
+            src={`https://www.youtube.com/embed/${videoId}`}
+            title={title}
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+const Contents = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchLatestVideos = async () => {
+      const latestVideos = await fetchVideos();
+      setVideos(latestVideos);
     };
 
-    return (
-        <div className="flex justify-center w-[100%] align-middle column">
+    fetchLatestVideos();
+  }, []);
 
-            <motion.div variants={slideIn("right", "tween", 0.2, 1)} className='flex-[0.25] w-[90%]'>
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} `}>My content</p>
+        <h2 className={`${styles.sectionHeadText}`}>Videos.</h2>
+      </motion.div>
 
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Check out my videos to learn more about my work and interests. Each
+          video showcases a different aspect of my skills and experience, and
+          provides real-world examples of my work. You can find links to the
+          code repositories and live demos in the video descriptions.
+        </motion.p>
+      </div>
 
-<YouTubePlaylist
-      apiKey="AIzaSyBbChToYFyckDdNxxawgP3zSbFYuDtpmsM"
-      playlistId="PLl06sy6-giMTuQKR6aODHLHqkF0rOH20o"
-      uniqueName="videos"
-    />
+      <div className="mt-20 flex flex-wrap gap-7">
+        {videos.map((video, index) => (
+          <VideoCard key={`video-${index}`} index={index} {...video} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-                </motion.div>
-        </div>
-    );
-}
-
-export default Contents;
+export default SectionWrapper(Contents, "");
