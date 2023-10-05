a72aaf7a0d1b4eba797c53ddee0b83f58f9f5a3f
Simon Ferns <122154257+AdonisCodes@users.noreply.github.com>
2023-10-05 10:20:00 +0200

    ignoring errors

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Continue on build errors (not recommended for production)
+    // This allows the build to continue even if there are errors.
+    // It suppresses error messages during the build.
+    // Use with caution.
+    continueOnError: true,
+
+    // Suppress error logging during build (not recommended for production)
+    // This suppresses error messages from being displayed in the console.
+    // Use with caution, as it can make debugging issues more challenging.
+    silent: true,
+  },
 })
